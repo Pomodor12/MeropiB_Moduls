6707a99eaa7e36c4afe266ced22b1f440c10dcf5
Pomodor12 <167606195+Pomodor12@users.noreply.github.com>
2025-12-03 22:47:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from patch_events import dp as dp_events
 from patch_notes import dp as dp_notes
-from patch_reminders import dp as dp_reminders, set_bot, check_urgent_events
+from patch_reminders import set_bot, check_urgent_events  # убрали dp
 
 TOKEN = "7999077800:AAGAlfz6ho1xAP2spR8k_18rGy4CPdWRo3k"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# Передаем bot в патчи
 set_bot(bot)
 
+# Подключаем маршрутизаторы
 dp.include_router(dp_events)
 dp.include_router(dp_notes)
-dp.include_router(dp_reminders)
 
 async def main():
     try:
