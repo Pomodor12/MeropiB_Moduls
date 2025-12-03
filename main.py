3de6ae7d409958c6052061b4126058a4f158e4de
Pomodor12 <167606195+Pomodor12@users.noreply.github.com>
2025-12-03 22:35:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from patch_events import dp as dp_events
 from patch_notes import dp as dp_notes
-from patch_reminders import dp as dp_reminders, set_bot
+from patch_reminders import dp as dp_reminders, set_bot, check_urgent_events
 
-# Твой токен
 TOKEN = "7999077800:AAGAlfz6ho1xAP2spR8k_18rGy4CPdWRo3k"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Передаем bot во все патчи
 set_bot(bot)
 
-# Подключаем все маршрутизаторы
 dp.include_router(dp_events)
 dp.include_router(dp_notes)
 dp.include_router(dp_reminders)
@@ -21,10 +18,13 @@ dp.include_router(dp_reminders)
 async def main():
     try:
         print("Бот запущен")
+        # запускаем срочные уведомления после инициализации бота
+        check_urgent_events()
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
